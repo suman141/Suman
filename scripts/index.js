6fd1a88f17643a798f9f07d31dca9db2863cb018
Suman <73638722+suman141@users.noreply.github.com>
2020-10-29 15:17:39 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Nov 14, 2020',
+  name: 'Bidisha'
 };
 
 function hideEverything() {
